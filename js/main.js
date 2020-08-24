bc84693bcd432ffd0caded9856848eab85ca6308
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-24 00:52:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+window.addEventListener('load', init);
+
+//DOM Elements
 const BROSWERSUPPORT = document.querySelector('.banner-support');
 
 //Browser Support Banner
 setTimeout(() => {BROSWERSUPPORT.style.display = 'none'}, 5.0*1000);
+
+//Start Game
+function init() {
+
+}
