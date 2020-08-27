225fe8a8aaa27fa8d8ce20a411050d8250f2e8fd
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-27 01:36:40 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,5 +8,35 @@ setTimeout(() => {BROSWERSUPPORT.style.display = 'none'}, 5.0*1000);
 
 //Start Game
 function init() {
+ //Show number of seconds in UI
+ 
+ //Load word from array and dictionary API
+ 
+ //Start matching words input
+ 
+ //Call countdown every second
+ setInterval(countdown, 1000);
+ //Check game status
+ setInterval(checkStatus, 50);
+}
+
+//Start matching words
+function startMatch() {
+
+}
+
+//Match currentWord to wordInput
+function
+
+//Pick & show random word from array or API
+function
+
+//Countdwon timer
+function countdown() {
+ 
+}
+
+//Check status of game
+function checkStatus() {
 
 }
